f6d4ff0da27f4d7955a742d77723a7f53fab4e06
PrateekSab <prateeksab@gmail.com>
2021-10-24 15:55:18 -0400

    Finalizing tests

--- test/TestInflationToken.js
+++ test/TestInflationToken.js
@@ -5,31 +5,25 @@ const { expectRevert } = require('@openzeppelin/test-helpers');
 
 const totalSupplyBigNumberHex = ethers.BigNumber.from("20000000000000000000000");
 
-describe("TestToken state and transactions", function () {
+describe("INFLATION  state and transactions", function () {
   let MyTestToken, testToken, admin, firstComer; 
 
   beforeEach(async () => {
     // this is connecting to localhost 8545
     provider = ethers.getDefaultProvider();
 
-    MyTestToken = await ethers.getContractFactory("MyTestContract");
+    MyTestToken = await ethers.getContractFactory("InflationToken");
     testToken = await MyTestToken.deploy();
 
-    //[owner, firstComer, secondComer, _] = await ethers.getSigners();
     const accounts = await ethers.getSigners();
     admin = accounts[0];
     firstComer = accounts[1];
-    //console.log("Admin adress:", admin);
-    //console.log("firstcomer adress:", firstComer);
-    /*for (const account of accounts) {
-      console.log(account.address);
-    }*/
   });
 
   it("Should test 'totalSupply' and other default values.", async function () {
     // 2.
-    expect(await testToken.symbol()).to.equal("TTY");
-    expect(await testToken.name()).to.equal("Testy");
+    expect(await testToken.symbol()).to.equal("INF");
+    expect(await testToken.name()).to.equal("InflationToken");
     expect(await testToken.decimals()).to.equal(18);
 
     const totalSupply = await testToken.totalSupply();
@@ -37,23 +31,30 @@ describe("TestToken state and transactions", function () {
   });
 
   it("Should test 'transfer' from the owner to first Transfer.", async function () {
-    console.log("Admin adress:", admin);
-    console.log("firstcomer adress:", firstComer);
+    console.log("Admin adress:", admin.toString());
+    console.log("firstcomer adress:", firstComer.toString());
     // From a user to another user
     let preAdminBalance = await testToken.balanceOf(admin.address);
     console.log('preadminbalance:',preAdminBalance.toLocaleString());
-    const amountToTransfer = 100;
+
+    const amountToTransfer = ethers.BigNumber.from(100).pow(10,18);
     await testToken.transfer(firstComer.address, amountToTransfer);
 
     let firstComerBalance = await testToken.balanceOf(firstComer.address);
-    const amountDeposit = amountToTransfer * (1 / 100) + amountToTransfer
+    const amountDeposit = amountToTransfer.mul(101).div(100)
+    // Check if 1 % inflation works for new user
     expect(firstComerBalance.toString()).to.equal(amountDeposit.toString());
+
     let adminBalance = await testToken.balanceOf(admin.address);
-    console.log('admin Deposit:', adminBalance.toLocaleString());
-    const adminRemainingBalance = await testToken.totalSupply() - amountDeposit; 
-    console.log('totalsupply:', await testToken.totalSupply());
-    console.log(adminRemainingBalance.toLocaleString());
-    //expect(adminBalance.toString()).to.equal(adminRemainingBalance.toString());
+    console.log('admin Deposit:', adminBalance.toString());
+
+    // Calculate the balance through first principles to check
+    let postTransferAdminBalance = preAdminBalance.sub(amountToTransfer)
+    let postTransferAdminBalanceWithInflation = postTransferAdminBalance.mul(101).div(100);
+
+    console.log(`Post transfer admin balance with Inflation: ${postTransferAdminBalanceWithInflation.toString()}`);
+    
+    expect(postTransferAdminBalanceWithInflation.toString()).to.equal(adminBalance.toString());
   });
 
 });
